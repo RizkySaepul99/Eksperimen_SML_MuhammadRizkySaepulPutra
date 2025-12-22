cf765a1f6c7892ae0c2bcb680228bb7f170a0018
Muhammad Rizky Saepul Putra <m.rizky.saepul.putra@gmail.com>
2025-12-22 19:06:03 +0700

    Add files via upload

---
+++ preprocessing/automate_MuhammadRizkySaepulPutra.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def load_data(path):
+    return pd.read_csv(path)
+
+def preprocess_data(df):
+    df = df.loc[~df['Position'].str.contains('GK', na=False)].copy()
+
+    df = df.drop(columns=[
+        'Alternative positions', 'GK Handling', 'GK Kicking',
+        'GK Positioning', 'GK Reflexes', 'GK Diving',
+        'url', 'card', 'play style', 'Height', 'Weight',
+        'ID', 'Rank', 'Name', 'GENDER', 'Position',
+        'Preferred foot', 'Nation', 'League', 'Team', 'Age'
+    ])
+
+    return df
+
+def save_data(df, output_path):
+    df.to_csv(output_path, index=False)
+
+def run_all_steps():
+    df = load_data("Eksperimen_SML_MuhammadRizkySaepulPutra/EAFC26_raw.csv")
+    df_clean = preprocess_data(df)
+    save_data(df_clean, "Eksperimen_SML_MuhammadRizkySaepulPutra/preprocessing/EAFC26_preprocessing.csv")
+    return df_clean
+
+if __name__ == "__main__":
+    run_all_steps()
